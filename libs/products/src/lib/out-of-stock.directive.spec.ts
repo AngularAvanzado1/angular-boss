48587d1d070c9edf0339a31337d47ed0bec06943
Alberto Basalo <albertobasalo@hotmail.com>
2019-09-12 14:00:47 +0200

    video

---
+++ libs/products/src/lib/out-of-stock.directive.spec.ts
@@ -0,0 +1,8 @@
+import { OutOfStockDirective } from './out-of-stock.directive';
+
+describe('OutOfStockDirective', () => {
+  it('should create an instance', () => {
+    const directive = new OutOfStockDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductTemplateComponent } from './product-template/product-template.component';
+import { OutOfStockDirective } from './out-of-stock.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ProductTemplateComponent],
-  exports: [ProductTemplateComponent]
+  declarations: [ProductTemplateComponent, OutOfStockDirective],
+  exports: [ProductTemplateComponent, OutOfStockDirective]
 })
 export class ProductsModule {}
