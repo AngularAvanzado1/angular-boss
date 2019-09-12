af4754d6e63be1827e45eab4bd5b43eb4a5e04b6
Alberto Basalo <albertobasalo@hotmail.com>
2019-09-12 17:52:48 +0200

    3_3 video pipes

---
+++ libs/products/src/lib/ex-rate.pipe.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Pipe, PipeTransform } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map, refCount, shareReplay } from 'rxjs/operators';
+
+@Pipe({
+  name: 'exRate'
+})
+export class ExRatePipe implements PipeTransform {
+  private readonly euroDollars = 1.13;
+  private readonly ratesApi = 'https://api.exchangeratesapi.io/latest?symbols=';
+
+  constructor(private httpClient: HttpClient) {}
+
+  public transform(euros: number, symbol: string): number | Observable<number> {
+    if (!symbol) {
+      return euros * this.euroDollars;
+    } else {
+      return this.getOnlineRates$(symbol).pipe(map(rate => euros * rate));
+    }
+  }
+
+  private getOnlineRates$(symbol: string) {
+    const ratesUrl = this.ratesApi + symbol;
+    return this.httpClient.get<any>(ratesUrl).pipe(
+      shareReplay(1),
+      refCount(),
+      map(resp => resp.rates[symbol])
+    );
+  }
+}
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductTemplateComponent } from './product-template/product-template.component';
 import { OutOfStockDirective } from './out-of-stock.directive';
+import { ExRatePipe } from './ex-rate.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ProductTemplateComponent, OutOfStockDirective],
-  exports: [ProductTemplateComponent, OutOfStockDirective]
+  declarations: [ProductTemplateComponent, OutOfStockDirective, ExRatePipe],
+  exports: [ProductTemplateComponent, OutOfStockDirective, ExRatePipe]
 })
 export class ProductsModule {}
