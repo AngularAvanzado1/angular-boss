c6ae2b1630958523f7f965995faf0185de102980
Alberto Basalo <albertobasalo@hotmail.com>
2019-10-07 13:01:41 +0200

    feat(7-ssr): ng add @nguniversal/express-engine --clientProject shop

--- apps/shop/src/app/app.module.ts
+++ apps/shop/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     RouterModule.forRoot(
       [
         {
